0da7a246999b0273cbfa9add6ddb1e4b5c7a2845
ken <kjmac123@gmail.com>
2014-12-11 00:10:00 +0000

    WIP clang and Linux. We're okay as long as we don't try and use the C++ stdlib it seems (linker problems)

---
+++ metabuilder/gnumakeclang_posix.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+TOPDIR=.
+
+pushd ${SCRIPTDIR}
+pwd
+${TOPDIR}/bin/metabuilder --input metabuilder.lua --gen gnumakeclang_posix --metabase ${TOPDIR}/metabase --outdir metatmp
+popd
+
---
+++ metabuilder/metabase/gnumakeclang_posix.lua
@@ -0,0 +1,4 @@
+metabase "gnumakeclang_posix"
+	import "gnumakeclang_common.lua"
+	import "platform_posix.lua"
+metabase_end()
