bad70f8d3d284957aaab588cb28b37ed1e5d9cb4
ken <kjmac123@gmail.com>
2014-12-13 22:14:08 +0000

    freebsd support

--- metabuilder/examples/helloworld/gnumakeclang_posix.sh
+++ metabuilder/examples/helloworld/gnumakeclang_posix.sh
@@ -1,10 +1,4 @@
-#!/bin/bash
-set -e
-SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 TOPDIR=../..
 
-pushd ${SCRIPTDIR}
-pwd
 ${TOPDIR}/bin/metabuilder --input metabuilder.lua --gen gnumakeclang_posix --metabase ${TOPDIR}/metabase --outdir metatmp
-popd
 
--- metabuilder/gnumakeclang_posix.sh
+++ metabuilder/gnumakeclang_posix.sh
@@ -1,10 +1,3 @@
-#!/bin/bash
-set -e
-SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 TOPDIR=.
-
-pushd ${SCRIPTDIR}
-pwd
 ${TOPDIR}/bin/metabuilder --input metabuilder.lua --gen gnumakeclang_posix --metabase ${TOPDIR}/metabase --outdir metatmp
-popd
 
