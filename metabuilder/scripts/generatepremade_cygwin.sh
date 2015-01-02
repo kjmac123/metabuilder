2dec4183ff431540a5b025718527e40434d6dd33
kmacleod <kjmac123@gmail.com>
2015-01-02 17:08:56 +0000

    WIP

--- metabuilder/scripts/generatepremade_cygwin.sh
+++ metabuilder/scripts/generatepremade_cygwin.sh
@@ -6,12 +6,19 @@ MBBIN=${TOPDIR}/bin/metabuilder
 
 pushd ${TOPDIR}
 
-${MBBIN} --input metabuilder.lua --gen gnumakegcc_cygwin --metabase metabase --outdir metatmp
-${MBBIN} --input metabuilder.lua --gen gnumakegcc_posix --metabase metabase --outdir metatmp
+${MBBIN} --input metabuilder.lua --gen gnumakegcc_cygwin --metabase metabase --outdir metatmp --endstyle unix
+${MBBIN} --input metabuilder.lua --gen gnumakegcc_mingw --metabase metabase --outdir metatmp --endstyle unix
+${MBBIN} --input metabuilder.lua --gen gnumakegcc_posix --metabase metabase --outdir metatmp --endstyle unix
 
-${MBBIN} --input metabuilder.lua --gen gnumakeclang_cygwin --metabase metabase --outdir metatmp
-${MBBIN} --input metabuilder.lua --gen gnumakeclang_posix --metabase metabase --outdir metatmp
+${MBBIN} --input metabuilder.lua --gen gnumakeclang_cygwin --metabase metabase --outdir metatmp --endstyle unix
+${MBBIN} --input metabuilder.lua --gen gnumakeclang_mingw --metabase metabase --outdir metatmp --endstyle unix
+${MBBIN} --input metabuilder.lua --gen gnumakeclang_posix --metabase metabase --outdir metatmp --endstyle unix
 
-cp -rfv metatmp/* premade
+${MBBIN} --input metabuilder.lua --gen msvc2010_windows --metabase metabase --outdir metatmp_premade --endstyle windows
+${MBBIN} --input metabuilder.lua --gen msvc2012_windows --metabase metabase --outdir metatmp_premade --endstyle windows
+${MBBIN} --input metabuilder.lua --gen msvc2013_windows --metabase metabase --outdir metatmp_premade --endstyle windows
+${MBBIN} --input metabuilder.lua --gen msvc2015_windows --metabase metabase --outdir metatmp_premade --endstyle windows
+
+cp -rfv metatmp_premade/* premade
 
 popd
--- metabuilder/scripts/generatepremade_windows.bat
+++ metabuilder/scripts/generatepremade_windows.bat
@@ -3,18 +3,18 @@ setlocal
 SET MBBIN=bin\windows\metabuilder
 
 pushd ..\
-REM %MBBIN% --input metabuilder.lua --gen gnumakegcc_cygwin --metabase metabase --outdir metatmp
-REM %MBBIN% --input metabuilder.lua --gen gnumakegcc_windows --metabase metabase --outdir metatmp
-REM %MBBIN% --input metabuilder.lua --gen gnumakegcc_posix --metabase metabase --outdir metatmp
+%MBBIN% --input metabuilder.lua --gen gnumakegcc_cygwin --metabase metabase --outdir metatmp_premade --endstyle unix
+%MBBIN% --input metabuilder.lua --gen gnumakegcc_mingw --metabase metabase --outdir metatmp_premade --endstyle unix
+%MBBIN% --input metabuilder.lua --gen gnumakegcc_posix --metabase metabase --outdir metatmp_premade --endstyle unix
 
-REM %MBBIN% --input metabuilder.lua --gen gnumakeclang_cygwin --metabase metabase --outdir metatmp
-REM %MBBIN% --input metabuilder.lua --gen gnumakeclang_windows --metabase metabase --outdir metatmp
-REM %MBBIN% --input metabuilder.lua --gen gnumakeclang_posix --metabase metabase --outdir metatmp
+%MBBIN% --input metabuilder.lua --gen gnumakeclang_cygwin --metabase metabase --outdir metatmp_premade --endstyle unix
+%MBBIN% --input metabuilder.lua --gen gnumakeclang_mingw --metabase metabase --outdir metatmp_premade --endstyle unix
+%MBBIN% --input metabuilder.lua --gen gnumakeclang_posix --metabase metabase --outdir metatmp_premade --endstyle unix
 
-%MBBIN% --input metabuilder.lua --gen msvc2010_windows --metabase metabase --outdir metatmp
-%MBBIN% --input metabuilder.lua --gen msvc2012_windows --metabase metabase --outdir metatmp
-%MBBIN% --input metabuilder.lua --gen msvc2013_windows --metabase metabase --outdir metatmp
-%MBBIN% --input metabuilder.lua --gen msvc2015_windows --metabase metabase --outdir metatmp
+%MBBIN% --input metabuilder.lua --gen msvc2010_windows --metabase metabase --outdir metatmp_premade
+%MBBIN% --input metabuilder.lua --gen msvc2012_windows --metabase metabase --outdir metatmp_premade
+%MBBIN% --input metabuilder.lua --gen msvc2013_windows --metabase metabase --outdir metatmp_premade
+%MBBIN% --input metabuilder.lua --gen msvc2015_windows --metabase metabase --outdir metatmp_premade
 
-robocopy metatmp premade /s
+robocopy metatmp_premade premade /s
 popd
