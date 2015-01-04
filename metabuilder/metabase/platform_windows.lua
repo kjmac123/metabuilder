23908b160a21f6a7a09241517efe6d26502c7df1
kmacleod <kjmac123@gmail.com>
2015-01-04 21:50:07 +0000

    MSVC2012 Express support working. x64 windows builds working. New windows exe

--- metabuilder/metabase/platform_windows.lua
+++ metabuilder/metabase/platform_windows.lua
@@ -31,6 +31,22 @@ libdirs
 	"!$(DXSDK_DIR)Lib\\x86",
 }
 
+libs 
+{
+	"kernel32.lib",
+	"user32.lib",
+	"gdi32.lib",
+	"winspool.lib",
+	"comdlg32.lib",
+	"advapi32.lib",
+	"shell32.lib",
+	"ole32.lib",
+	"oleaut32.lib",
+	"uuid.lib",
+	"odbc32.lib",
+	"odbccp32.lib",
+}
+
 config "Debug"
 	defines 
 	{
