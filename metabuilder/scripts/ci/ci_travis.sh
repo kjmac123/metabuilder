08ab40a6b2da1563a77d49f82c0e63202275f31f
kmacleod <kjmac123@gmail.com>
2015-01-09 00:29:33 +0000

    WIP CI

---
+++ metabuilder/scripts/ci/ci_travis.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+TOPDIR=${SCRIPTDIR}/..
+
+GEN=gnumakegcc_posix
+
+if [ "${CXX}" = "clang++" ]; then
+   GEN=gnumakeclang_posix
+fi
+            
+pushd ${TOPDIR}/premade/metabuilder/${GEN}/
+make clean BUILDCONFIG=Release
+make -j4 BUILDCONFIG=Release
+
+cp out/metabuilder/Release/metabuilder ${TOPDIR}/bin
+${TOPDIR}/generatepremade_allplatforms_posix.sh
+
+popd
+
+
