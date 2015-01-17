59390813059ebe795c78e384db4e028afb775f33
kmacleod <kjmac123@gmail.com>
2015-01-17 15:32:00 +0000

    Adding OSX gnumake bootstrap

--- metabuilder/bootstrap_gnumakeclang_osx.sh
+++ metabuilder/bootstrap_gnumakeclang_osx.sh
@@ -4,4 +4,4 @@ pushd premade/metabuilder/gnumakeclang_osx
 make BUILDCONFIG=Release
 popd
 
-cp -v premade/metabuilder/gnumakeclang_posix/out/metabuilder/Release/metabuilder bin/osx/x86_64/metabuilder/
+cp -v premade/metabuilder/gnumakeclang_osx/out/metabuilder/Release/metabuilder bin/osx/x86_64/
